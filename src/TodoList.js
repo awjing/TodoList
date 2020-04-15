9b77a74b0afe28a02bd630150d2dc03f4bd85a08
wangjing <wangjing90@jd.com>
2020-04-15 23:29:25 +0800

    redux -- Todolist删除功能

--- src/TodoList.js
+++ src/TodoList.js
@@ -29,7 +29,7 @@ class TodoList extends Component {
           style={{marginTop: '10px', width: '300px'}}
           bordered
           dataSource={this.state.list}
-          renderItem={item => <List.Item>{item}</List.Item>}
+          renderItem={(item, index) => <List.Item onClick={this.handleItemClick.bind(this, index)}>{item}</List.Item>}
         />
       </div>
     )
@@ -50,6 +50,14 @@ class TodoList extends Component {
   handleBtnClick () {
     const action = {
       type: 'add_todo_item'
+    }
+    store.dispatch(action)
+  }
+
+  handleItemClick (index) {
+    const action = {
+      type: 'delete_todo_item',
+      index
     }
     store.dispatch(action)
   }
